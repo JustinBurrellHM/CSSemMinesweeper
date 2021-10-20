2210dd8a06ad2f0109e7a841046393c41d9328f2
Diane <dianekim120@gmail.com>
2021-10-20 09:10:54 -0400

    first commit

---
+++ Minesweeper.py
@@ -0,0 +1,24 @@
+import random
+
+
+class Minesweeper:
+    def __init__(self, n, m, k):
+        self.n = 0
+        self.m = 0
+        self.k = 0
+
+    def difficulty(x):
+        diff = input("choose a difficulty: ")
+        if diff ==
+
+    def generateMines(n, m, k):
+
+        for i in k:
+            random.randint(n)
+
+    def generateBoard(n, m):
+        array = [[0 for i in range(n)] for i in range(m)]
+        print(array)
+
+    def printBoard():
+        pass
