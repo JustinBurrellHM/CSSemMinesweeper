e09d9795636af7b881b16f192837def540db13d5
Justin Burrell <justinburrell715@gmail.com>
2021-10-20 20:04:49 -0400

    second commit

--- minesweeper.py
+++ minesweeper.py
@@ -7,15 +7,19 @@ class Minesweeper:
 
     def difficulty(x):
         diff = input("choose a difficulty: ")
-        if diff ==
+        if diff == 
 
     def generateMines(n, m, k):
 
         for i in k:
             random.randint(n)
 
-    def generateBoard(n, m):
+    def generateBoard(n, m, k):
         array = [[0 for i in range(n)] for i in range(m)]
+
+        for i in range(0,k):
+            x, y = random.randint(0,n-1), random.randint(0,m-1)
+            array[x][y] = "M"
         print(array)
 
     def printBoard():
