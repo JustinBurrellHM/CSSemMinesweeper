1e37ba437fcc9676e4ed77595427cbc3a51d08e5
Diane <dianekim120@gmail.com>
2021-10-21 08:57:11 -0400

    x, y coords

--- justins_minesweeper_sandbox.py
+++ justins_minesweeper_sandbox.py
@@ -1,15 +1,18 @@
 import random
-def generate_board(n,m,k):
+
+
+def generate_board(n, m, k):
     array = [[0 for i in range(n)] for i in range(m)]
-    for i in range(0,k):
-        x, y = random.randint(0,n-1), random.randint(0,m-1)
-        while array[x][y] == "M":
-            x, y = random.randint(0,n-1), random.randint(0,m-1)
+    for i in range(0, k):
+        y, x = random.randint(0, n-1), random.randint(0, m-1)
+        while array[x][y] == "M" & x:
+            y, x = random.randint(0, n-1), random.randint(0, m-1)
         array[x][y] = "M"
 
     return array
 
-game_board = generate_board(5,6,3)
+
+game_board = generate_board(5, 6, 3)
 
 # def count_mines(a):
 
@@ -23,4 +26,3 @@ def print_board(a):
 
 
 print(print_board(game_board))
-
