577c6bb89252d373d384e4bbd28a8db88fb06bac
Justin Burrell <justinburrell715@gmail.com>
2021-10-26 09:14:46 -0400

    i hate it here

--- minesweeper.py
+++ minesweeper.py
@@ -2,34 +2,34 @@ import random
 
 class Array:
     def __init__(self):
-        self.n = 0 # num columns
-        self.m = 0 # num rows 
+        self.c = 0 # num columns
+        self.r = 0 # num rows 
         self.k = 0 # num of mines
         self.array = []
         self.set_difficulty()    
     def set_difficulty(self):
         difficulty = input("What difficult do you want: Easy, Medium, or Hard? ")
         if difficulty == "Easy":
-            self.n = 5   # 5 columns and 6 rows
-            self.m = 6
+            self.c = 5   # 5 columns and 6 rows
+            self.r = 6
             self.k = 3
-            self.array = [[0 for i in range(self.n)] for i in range(self.m)]
+            self.array = [[0 for i in range(self.c)] for i in range(self.r)]
         elif difficulty == "Medium":
-            self.n = 8
-            self.m = 8
+            self.c = 8
+            self.r = 8
             self.k = 6
-            self.array = [[0 for i in range(self.n)] for i in range(self.m)]
+            self.array = [[0 for i in range(self.c)] for i in range(self.r)]
         elif difficulty == "Hard":
-            self.n = 12
-            self.m = 12
+            self.c = 12
+            self.r = 12
             self.k = 12
-            self.array = [[0 for i in range(self.n)] for i in range(self.m)]
+            self.array = [[0 for i in range(self.c)] for i in range(self.r)]
         
     def generate_board(self):
         for i in range(0, self.k):
-            y, x = random.randint(0, self.n-1), random.randint(0, self.m-1)
+            y, x = random.randint(0, self.c-1), random.randint(0, self.r-1)
             while self.array[x][y] == "M":
-                y, x = random.randint(0, self.n-1), random.randint(0, self.m-1)
+                y, x = random.randint(0, self.c-1), random.randint(0, self.r-1)
             self.array[x][y] = "M"
         return self.array
 
@@ -38,12 +38,18 @@ class Array:
         counter = 0
         # iterate through all the potential points
         for d in range(m-1, m+2): # on easy, n = 5 but d is zero-indexed; it has to be from n-2 to n+1 to correspond to the right indices
-            print('d', d)
             for c in range(n-1, n+2):
-                print('c', c)
-                if self.array[d][c] == "M":
-                    counter += 1
+                if self.in_bounds(d,c):
+                    if self.array[d][c] == "M":
+                        counter += 1
         self.array[n][m] = counter
+    
+    def in_bounds(self, d, c):
+        if d in range(self.r):
+            if c in range(self.c):
+                return True
+        else:
+            return False
     
     def count_mines(self, x, y):
         for a in range(self.array[y]):
@@ -60,8 +66,8 @@ class Array:
 if __name__ == "__main__":
     array = Array()
     array.generate_board()
-    array.check_mines(array.n, array.m)
-    array.count_mines(array.n, array.m)
+    array.check_mines(array.c, array.r)
+    array.count_mines(array.c, array.r)
     array.print_board()
 
 
